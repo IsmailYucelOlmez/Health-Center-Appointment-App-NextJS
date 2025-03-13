cb43a5a23a296ad1cb70964861f7047448f02698
İsmail Yücel Ölmez <ismailyucelolmez514@gmail.com>
2025-03-14 01:32:58 +0300

    Home Page Design and Registration Form Implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,50 @@
-import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import PasskeyModal from "@/components/PassKeyModal";
+import { PatientForm } from "@/components/forms/PatientForm";
 
+type SearchParamProps = {
+  searchParams: {
+    admin: string;
+  };
+};
 
-export default function Home() {
+const Home = ({ searchParams }: SearchParamProps) => {
+  const isAdmin = searchParams?.admin === "true";
+
   return (
-    <div className=" ">
-      <p>Home</p>
-      <Button>Login</Button>
+    <div className="flex h-screen max-h-screen">
+      {isAdmin && <PasskeyModal />}
+
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <h1 className="text-xl font-bold text-dark-800 mb-10">
+          <span className="text-green-500">Sağlık Merkezi</span> Randevu Sistemi
+          </h1>
+          
+
+          <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 text-sm xl:text-left">
+              © 2025 Copyright
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
-}
+};
+
+export default Home;
---
+++ src/lib/validation.ts
@@ -0,0 +1,96 @@
+import { z } from "zod";
+
+export const UserFormValidation = z.object({
+  name: z
+    .string()
+    .min(2, "Name must be at least 2 characters")
+    .max(50, "Name must be at most 50 characters"),
+  email: z.string().email("Invalid email address"),
+  phone: z
+    .string()
+    .refine((phone) => /^\+\d{10,15}$/.test(phone), "Invalid phone number"),
+});
+
+export const PatientFormValidation = z.object({
+  name: z
+    .string()
+    .min(2, "Name must be at least 2 characters")
+    .max(50, "Name must be at most 50 characters"),
+  email: z.string().email("Invalid email address"),
+  phone: z
+    .string()
+    .refine((phone) => /^\+\d{10,15}$/.test(phone), "Invalid phone number"),
+  gender: z.enum(["Male", "Female", "Other"]),
+  occupation: z
+    .string()
+    .min(2, "Occupation must be at least 2 characters")
+    .max(500, "Occupation must be at most 500 characters"),
+  primaryPhysician: z.string().min(2, "Select at least one doctor"),
+  insuranceProvider: z
+    .string()
+    .min(2, "Insurance name must be at least 2 characters")
+    .max(50, "Insurance name must be at most 50 characters"),
+  currentMedication: z.string().optional(),
+  identificationType: z.string().optional(),
+  identificationNumber: z.string().optional(),
+  identificationDocument: z.custom<File[]>().optional(),
+  treatmentConsent: z
+    .boolean()
+    .default(false)
+    .refine((value) => value === true, {
+      message: "You must consent to treatment in order to proceed",
+    }),
+  disclosureConsent: z
+    .boolean()
+    .default(false)
+    .refine((value) => value === true, {
+      message: "You must consent to disclosure in order to proceed",
+    }),
+  privacyConsent: z
+    .boolean()
+    .default(false)
+    .refine((value) => value === true, {
+      message: "You must consent to privacy in order to proceed",
+    }),
+});
+
+export const CreateAppointmentSchema = z.object({
+  primaryPhysician: z.string().min(2, "Select at least one doctor"),
+  schedule: z.coerce.date(),
+  reason: z
+    .string()
+    .min(2, "Reason must be at least 2 characters")
+    .max(500, "Reason must be at most 500 characters"),
+  note: z.string().optional(),
+  cancellationReason: z.string().optional(),
+});
+
+export const ScheduleAppointmentSchema = z.object({
+  primaryPhysician: z.string().min(2, "Select at least one doctor"),
+  schedule: z.coerce.date(),
+  reason: z.string().optional(),
+  note: z.string().optional(),
+  cancellationReason: z.string().optional(),
+});
+
+export const CancelAppointmentSchema = z.object({
+  primaryPhysician: z.string().min(2, "Select at least one doctor"),
+  schedule: z.coerce.date(),
+  reason: z.string().optional(),
+  note: z.string().optional(),
+  cancellationReason: z
+    .string()
+    .min(2, "Reason must be at least 2 characters")
+    .max(500, "Reason must be at most 500 characters"),
+});
+
+export function getAppointmentSchema(type: string) {
+  switch (type) {
+    case "create":
+      return CreateAppointmentSchema;
+    case "cancel":
+      return CancelAppointmentSchema;
+    default:
+      return ScheduleAppointmentSchema;
+  }
+}
